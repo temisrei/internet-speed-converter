2875a29c7faf9f98044eb1584ba71bd84899991b
Rei Lin <temisrei@gmail.com>
2021-01-16 22:30:46 +0800

    註冊 React FontAwesome 中會用到的圖示

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import UnitControl from './components/UnitControl';
 import CardFooter from "./components/CardFooter";
 import UnitConverter from './components/UnitConverter';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fab, fas, far);
 
 function App() {
   const [inputValue, setInputValue] = useState(0);

8db7dce02d862f5df9060b41b4c4b5a39cc2fba0
Rei Lin <temisrei@gmail.com>
2021-01-16 12:28:17 +0800

    使用 useState 建立 React 狀態

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState(0);
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setInputValue(value);
+  };
+
   return (
     <div className="container">
       <div className="card-header">Network Speed Converter</div>
@@ -17,14 +25,17 @@ function App() {
         <div className="converter">
           <div className="flex-1">
             <div className="converter-title">Set</div>
-            <input type="number" className="input-number" min="0" value="100" />
+            <input onChange={handleInputChange}
+              value={inputValue}
+              type="number" className="input-number" min="0" />
           </div>
           <span className="angle-icon fa-2x" style={{marginTop: '30px'}}>
             <i className="fas fa-angle-right"></i>
           </span>
           <div className="text-right flex-1">
             <div className="converter-title">Show</div>
-            <input type="text" className="input-number text-right" value="12.5" disabled />
+            <input value={inputValue / 8} 
+              type="text" className="input-number text-right" disabled />
           </div>
         </div>
       </div>

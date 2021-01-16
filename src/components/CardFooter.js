17bb02a971532afd6624173bf8a1871caa6048ed
Rei Lin <temisrei@gmail.com>
2021-01-16 15:28:38 +0800

    依據 inputValue 改變 CardFooter 內的文字內容和樣式

--- src/components/CardFooter.js
+++ src/components/CardFooter.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 const CardFooter = (props) => {
   const { inputValue } = props;
-  console.log(inputValue);
-  return (<div className="card-footer">FAST</div>);
+
+  let criteria = {};
+  if (!inputValue) {
+    criteria = {
+      title: '---',
+      backgroundColor: '#d3d8e2'
+    };
+  } else if (inputValue < 15) {
+    criteria = {
+      title: 'SLOW',
+      backgroundColor: '#ee362d'
+    };    
+  } else if (inputValue < 40) {
+    criteria = {
+      title: 'GOOD',
+      backgroundColor: '#1b82f1'
+    };     
+  } else if (inputValue >= 40) {
+    criteria = {
+      title: 'FAST',
+      backgroundColor: '#13d569'
+    };     
+  }
+
+  return (<div style={{ backgroundColor: criteria.backgroundColor }}
+    className="card-footer">{criteria.title}</div>);
 };
 
 export default CardFooter;
